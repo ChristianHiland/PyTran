f3bf7482b55ab8dc28db4a4b5b5d959f60e88368
Christian Hiland <hilandchristian112@gmail.com>
2023-08-02 14:40:10 -0400

    Update Run.py

--- PyTran/Run.py
+++ PyTran/Run.py
@@ -1,8 +1,20 @@
-import os
 from pathlib import Path
+import platform
+import json
+import os
 
 class Start:
   # The Init Func
   def __init__(self):
     self.HomeDIR = Path.cwd()
-    self.ConfigFile = str("PyTran/")
+    self.Username = os.getlogin()
+    self.OS = platform.uname()
+    self.ConfigFile = str("PyTran/Config.json")
+  # First Run
+  def First(self):
+    Layout = {
+      "WorkFolder": self.HomeDIR,
+      "Username": self.Username,
+      "OS": 
+    }
+    with open(self.ConfigFile, "w")
