b8389bcae5da10922eeb41a742f35b463b11bf02
Christian Hiland <hilandchristian112@gmail.com>
2023-08-02 09:19:45 -0400

    Update English.py

--- PyTran/English.py
+++ PyTran/English.py
@@ -4,11 +4,17 @@ import os
 class English:
     def __init__(self):
         self.JSONFile = str("PyTran/EnglishSen.json")
+        self.JSONWhole = str("PyTran/English.json")
         pass
-    def Translate(self, Word):
-        with open(self.JSONFile, "r") as Tran:
-            Data = json.load(Tran)
-            print(Data['EnglishSen'][Word])
+    def Translate(self, Word, Whole):
+        if Whole.lower() == str("n"):
+            with open(self.JSONFile, "r") as Tran:
+                Data = json.load(Tran)
+                print(Data['EnglishSen'][Word])
+        elif Whole.lower() == str("y"):
+            with open(self.JSONWhole, "r") as Tran:
+                Data = json.load(Tran)
+                print(Data['English'][Word])
     def Write(self, Dict):
         with open(self.JSONFile, "w") as Add:
             Data = json.load(Add)
