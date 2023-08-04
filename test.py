67dc68f01e270a9c89b6c6d13fc41e3310bf287c
Christian Hiland <hilandchristian112@gmail.com>
2023-08-04 16:41:15 +0000

    modified:   test.py

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ from PyTran import English
 T = English()
 
 Dict = {"love": "사랑", "사랑": "love", "my": "내", "내": "my", "coding": "코딩", "코딩": "coding"}
-T.Translate("Hello", "n")
+T.Translate("Hello", "y")
